c719684c9b2be59ec11c3224b65f8be1741afdc7
som <somprajapati24@gmail.com>
2025-08-16 18:26:40 +0530

    minor - commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,8 @@ import BubbleSort from "@/components/learning/sorting/BubbleSort";
 import SelectionSort from "@/components/learning/sorting/SelectionSort";
 import InsertionSort from "@/components/learning/sorting/InsertionSort";
 
-import AINotesPage from "@/components/learning/AINotes.tsx/AINotesPage";
+// import AINotesPage from "@/components/learning/AINotes.tsx/AINotesPage";
+import AINotesPage from "@/components/learning/AINotes.tsx/AINotes";
 import SortingVisualizerApp from "@/components/learning/AINotes.tsx/sortingVisualizerApp";
 import SortingVisualizerApp2 from "@/components/learning/AINotes.tsx/sortingVisualizerApp2";
 import SortingControls from "@/components/learning/sorting/SortingControl";
@@ -11,22 +12,22 @@ import SortingControls from "@/components/learning/sorting/SortingControl";
 export default function Home() {
   return (
     <>
-      {/* <div>
+      {/* {/* <div>
         <BubbleSort />
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <InsertionSort />
-      </div>
+      </div> */}
       <div>
         <SelectionSort />
       </div>
-      <div>
+      {/* <div>
         <SortingVisualizerApp />
-      </div> */}
+      </div>  */}
       {/* <div>
         <SortingVisualizerApp2 />
       </div> */}
-      <SelectionSort />
+      {/* <SelectionSort /> */}
 
       {/* <SortingControls /> */}
 
